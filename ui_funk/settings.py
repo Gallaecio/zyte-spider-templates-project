55866580c5ded73683b14b6fb68560eb7a9f7edc
Adrián Chaves <adrian@chaves.io>
2024-05-16 22:14:41 +0200

    Initial commit

---
+++ ui_funk/settings.py
@@ -0,0 +1,31 @@
+from itemadapter import ItemAdapter
+from zyte_common_items import ZyteItemAdapter
+
+ItemAdapter.ADAPTER_CLASSES.appendleft(ZyteItemAdapter)
+
+BOT_NAME = "ui_funk"
+
+SPIDER_MODULES = [
+    "zyte_spider_templates.spiders",
+    "ui_funk.spiders",
+]
+NEWSPIDER_MODULE = "ui_funk.spiders"
+
+CLOSESPIDER_TIMEOUT_NO_ITEM = 900
+ADDONS = {
+    "scrapy_zyte_api.Addon": 500,
+}
+SCHEDULER_DISK_QUEUE = "scrapy.squeues.PickleFifoDiskQueue"
+SCHEDULER_MEMORY_QUEUE = "scrapy.squeues.FifoMemoryQueue"
+SPIDER_MIDDLEWARES = {
+    "scrapy_poet.RetryMiddleware": 275,
+    "scrapy.spidermiddlewares.offsite.OffsiteMiddleware": None,
+    "zyte_spider_templates.middlewares.AllowOffsiteMiddleware": 500,
+    "zyte_spider_templates.middlewares.CrawlingLogsMiddleware": 1000,
+}
+
+# scrapy-poet
+SCRAPY_POET_DISCOVER = [
+    "zyte_spider_templates.pages",
+    "ui_funk.pages",
+]
